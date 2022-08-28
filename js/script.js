0955e4c049a792dafa8492dd6a6224dc9c4454b7
pavel-4 <pavel.chizhonok@gmail.com>
2022-08-28 22:54:09 +0300

    feat: implemeted responsive design to `slider` in `homes` section

--- js/script.js
+++ js/script.js
@@ -2,9 +2,35 @@ const swiper = new Swiper(".homes__slider", {
   slidesPerView: 4,
   spaceBetween: 16,
   slidesPerGroup: 1,
+  // grabbing by mouse
+  simulateTouch: true,
+  grabCursor: true,
   loop: true,
+  loopedSlides: 0,
   loopFillGroupWithBlank: true,
+  // switching by keyboard
+  keyboard: {
+    enabled: true,
+  },
+  // switching by mousewheel
+  mousewheel: {
+    sensivity: 1,
+  },
   navigation: {
     nextEl: ".homes__list_slider-btn",
+  },
+  breakpoints: {
+    360: {
+      slidesPerView: 2,
+    },
+    480: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
   },
 });
