fce69eb6a7a7dbd0d89b59d726f4ebb6d72ecd4e
pavel-4 <pavel.chizhonok@gmail.com>
2022-08-28 16:14:52 +0300

    feat: implemented slider to `homes` section (desktop)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-const swiper = new Swiper(".homes__slider");
+const swiper = new Swiper(".homes__slider", {
+  slidesPerView: 4,
+  spaceBetween: 16,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  navigation: {
+    nextEl: ".homes__list_slider-btn",
+  },
+});
