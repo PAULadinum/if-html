6d5deda3692a787377b3b03710b8ac4492cf8093
pavel-4 <71784588+pavel-4@users.noreply.github.com>
2022-09-18 12:36:48 +0300

    Lesson 13 - CSS Frameworks - Homework (#14)
    * fix: improved logo in `app` section
    
    * feat: added favicons for all platforms
    
    * feat: added titles to links & improved buttons
    
    * feat: installed bootstrap
    
    * feat: bootstrap linked to `index.html`
    
    * feat: changed classes in `nav-panel`
    
    * refactor: changed `top-section` (structure & classes)
    
    * refactor: changed structure of `sign-up` section
    
    * feat: installed `swiper`
    
    * refactor: changed structure of `homes` section
    
    * feat: implemented slider to `homes` section (desktop)
    
    * feat: implemeted responsive design to `slider` in `homes` section
    
    * fix: improved line-height & renamed classes for `destination` section according to BEM-methodology
    
    * refactor: changed clases in `reviews` section according to BEM-methodology
    
    * fix: improved position of slider button in `homes` section (exclude its overlapping)
    
    * fix: improved `navigation` panel
    
    * fix: improved alignment in `reviews` section
    
    * feat: trying to wrap text to content width in `homes` section
    
    * feat: trying to wrap text to content width in `homes` section (2)
    
    * fix: text displaying in `homes` section had been improved
    
    * feat: removed `app` section and improved `sign-up` section
    
    * refactor: changeg order in `queries.css`
    
    * fix: improved adaptive design in  section

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const swiper = new Swiper(".homes__slider", {
+  slidesPerView: 4,
+  spaceBetween: 16,
+  slidesPerGroup: 1,
+  // grabbing by mouse
+  simulateTouch: true,
+  grabCursor: true,
+  loop: true,
+  loopedSlides: 0,
+  loopFillGroupWithBlank: true,
+  // switching by keyboard
+  keyboard: {
+    enabled: true,
+  },
+  // switching by mousewheel
+  mousewheel: {
+    sensivity: 1,
+  },
+  navigation: {
+    nextEl: ".homes__slider-button",
+  },
+  breakpoints: {
+    360: {
+      slidesPerView: 2,
+    },
+    480: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
